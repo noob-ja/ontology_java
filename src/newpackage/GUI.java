4764231dad2839c3a44aad3e84be4d3f7543161a
yukang1996 <yukang_1996@hotmail.com>
2018-04-09 01:42:18 +0800

    change prompt

--- src/newpackage/GUI.java
+++ src/newpackage/GUI.java
@@ -38,12 +38,14 @@ public class GUI {
         button1.setLocation(10, 10);
         button1.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                String text;
-                text = JOptionPane.showInputDialog("Input Super Node");
-                conceptlist.setSuper(text);
-                conceptlist.setConceptGUI(text,1);
-                redraw();
+            public void actionPerformed(ActionEvent e) {               
+                    String text;
+                    text = JOptionPane.showInputDialog("Input Super Node");
+                    conceptlist.setSuper(text);
+                    conceptlist.setConceptGUI(text,1);
+                    redraw();
+                
+                
             }
         });
 //        frame.add(button1);
@@ -52,6 +54,23 @@ public class GUI {
         JButton button2 = new JButton("Add parent");
         button2.setSize(100,100);
         button2.setLocation(10, 110);
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(drawPanel.getSelected()==""){
+                    //prompt user to select a concept
+                     JOptionPane.showMessageDialog(null, "Please select a node.");
+                }else{
+                    String text;
+                    text = JOptionPane.showInputDialog("Input Parent Node");
+                    conceptlist.addParent(drawPanel.getSelected(), text);
+                   // conceptlist.setConceptGUI(text, conceptlist.search(drawPanel.getSelected()).getGUIData("level")+1);
+                  //conceptlist.setConceptGUI(text, conceptlist.search(drawPanel.getSelected()).getGUIData("level")+1);
+//                    checkOverlapping(conceptlist.getAll());
+                    redraw();
+                }
+            }
+        });
         
         //frame.add(button2);
         
@@ -61,13 +80,46 @@ public class GUI {
         button3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(drawPanel.getSelected()==null){
+                if(drawPanel.getSelected()==""){
                     //prompt user to select a concept
+                     JOptionPane.showMessageDialog(null, "Please select a node.");
                 }else{
                     String text;
                     text = JOptionPane.showInputDialog("Input Child Node");
                     conceptlist.addChild(drawPanel.getSelected(), text);
                     conceptlist.setConceptGUI(text, conceptlist.search(drawPanel.getSelected()).getGUIData("level")+1);
+//                    checkOverlapping(conceptlist.getAll());
+                    redraw();
+                }
+            }
+        });
+        
+        JButton button4 = new JButton("Delete current & child");
+        button4.setSize(100,100);
+        button4.setLocation(10, 210);
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(drawPanel.getSelected()==""){
+                    JOptionPane.showMessageDialog(null, "Please select a node.");
+                }else{
+                    conceptlist.removeBranch(drawPanel.getSelected());
+//                    checkOverlapping(conceptlist.getAll());
+                    redraw();
+                }
+            }
+        });
+        
+        JButton button5 = new JButton("Delete current");
+        button5.setSize(100,100);
+        button5.setLocation(10, 210);
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(drawPanel.getSelected()==""){
+                    JOptionPane.showMessageDialog(null, "Please select a node.");
+                }else{
+                    conceptlist.removeSelf(drawPanel.getSelected());
 //                    checkOverlapping(conceptlist.getAll());
                     redraw();
                 }
@@ -87,6 +139,12 @@ public class GUI {
         c.gridx = 0;
         c.gridy = 2;
         panelB.add(button3,c);
+        c.gridx = 0;
+        c.gridy = 3;
+        panelB.add(button4,c);
+        c.gridx = 0;
+        c.gridy = 4;
+        panelB.add(button5,c);
         frame.add(panelB,BorderLayout.WEST);
         
         redraw();
